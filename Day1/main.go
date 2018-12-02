f17e47e6c65b8ba21ce88b369c7af86e6c0fff9b
johnthomas1986 <john.thomas@maersk.com>
2018-12-02 21:06:54 +0100

    puzzle1 completed

--- Day1/main.go
+++ Day1/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Advent code challenge day 1")
+	f, err := os.Open("./Day1/frequency.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	var freq []int
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
+		freq = append(freq, i)
+	}
+
+	currentFreq := 0
+	r := resultingFrequency(freq, currentFreq)
+	fmt.Println("resulting freq", r)
+
+}
+
+func resultingFrequency(f []int, currentFreq int) int {
+	var changeFreq int
+	var rf int
+	for j := range f {
+		changeFreq = f[j]
+		rf = currentFreq + changeFreq
+		currentFreq = rf
+	}
+	return rf
 }
